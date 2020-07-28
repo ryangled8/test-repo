247cf8a8fcd45056155d598133e2c88852464c81
Ryan Gledhill <ryan@engageinteractive.co.uk>
2020-07-28 10:14:56 +0100

    Updates Site Header Title

--- resources/views/layouts/partials/site-header.blade.php
+++ resources/views/layouts/partials/site-header.blade.php
@@ -1,7 +1,7 @@
 <header class="relative z-site-header">
 	<div class="e-container">
 		<h1>
-			<a href="{{ $links['home'] }}">Project name</a>
+			<a href="{{ $links['home'] }}">Test repo</a>
 		</h1>
 	</div>
 </header>
